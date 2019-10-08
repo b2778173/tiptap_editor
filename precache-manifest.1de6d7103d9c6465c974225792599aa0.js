0fc331c44800e9b2cf137c9e763aef5b681ffc31
Kevin Tsai <b2778173@hotmail.com>
2019-10-08 17:37:29 +0800

    deploy

---
+++ precache-manifest.1de6d7103d9c6465c974225792599aa0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tiptap_editor/robots.txt"
+  },
+  {
+    "revision": "533320b32cb9d480facf",
+    "url": "/tiptap_editor/js/chunk-vendors.288d125d.js"
+  },
+  {
+    "revision": "f06c7c32bd3c878f26d9",
+    "url": "/tiptap_editor/js/app.90e061af.js"
+  },
+  {
+    "revision": "20a4a2b36b2524710386dad745e3509b",
+    "url": "/tiptap_editor/index.html"
+  },
+  {
+    "revision": "c80ca203c0f773a8502d85ae60f347ce",
+    "url": "/tiptap_editor/img/youtube-brands.c80ca203.svg"
+  },
+  {
+    "revision": "19102aeebe7f20d3cebe92b2b6507d9c",
+    "url": "/tiptap_editor/img/ul.19102aee.svg"
+  },
+  {
+    "revision": "fe1a5f990ee6cb688b7fa1d9a07568e2",
+    "url": "/tiptap_editor/img/ol.fe1a5f99.svg"
+  },
+  {
+    "revision": "cee3473229c5f145de6998c492199c8d",
+    "url": "/tiptap_editor/img/image.cee34732.svg"
+  },
+  {
+    "revision": "3bcc3ecffca85f1a7f387941b98d6fbb",
+    "url": "/tiptap_editor/img/bold.3bcc3ecf.svg"
+  },
+  {
+    "revision": "f06c7c32bd3c878f26d9",
+    "url": "/tiptap_editor/css/app.de89bdb2.css"
+  }
+];
